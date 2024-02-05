f09c0b27c4ad3f244fbfc9109b87b059d0a34e72
Daniil <dan_mishka@mail.ru>
2024-02-05 22:33:13 +1000

    Add draw hangman and add attribute status, add victory condition, add docstrings for methods

--- hangman_game.py
+++ hangman_game.py
@@ -2,7 +2,81 @@ from random import choice
 # GAME_THEME = "Животное"
 # test_tuple = (1, 2, 3, 4,"Hello", True)
 # test_tuple
-
+HANGMAN1_STR = (
+    "        $$$$$$$$$$$$$$$\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    " $$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+    " $                          $\n"
+)
+HANGMAN2_STR = (
+    "        $$$$$$$$$$$$$$$\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        O             $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    " $$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+    " $                          $\n"
+)
+HANGMAN3_STR = (
+    "        $$$$$$$$$$$$$$$\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        O             $\n"
+    "      /   \\           $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    " $$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+    " $                          $\n"
+)
+HANGMAN4_STR = (
+    "        $$$$$$$$$$$$$$$\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        O             $\n"
+    "      / | \\           $\n"
+    "        |             $\n"
+    "                      $\n"
+    "                      $\n"
+    "                      $\n"
+    " $$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+    " $                          $\n"
+)
+HANGMAN5_STR = (
+    "        $$$$$$$$$$$$$$$\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        |             $\n"
+    "        O             $\n"
+    "      / | \\           $\n"
+    "        |             $\n"
+    "       / \\            $\n"
+    "                      $\n"
+    "                      $\n"
+    " $$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+    " $                          $\n"
+)
 WORDS_TUPLE = (
     ("ХИМИЧЕСКИЙ ЭЛЕМЕНТ", "АРГОН"),
     ("ХИМИЧЕСКИЙ ЭЛЕМЕНТ", "АСТАТ"),
@@ -28,6 +102,15 @@ WORDS_TUPLE = (
 class HangmanGame:
     """
     Описывает класс игры виселицы
+
+
+    attributes:
+
+    health: Здоровье игрока
+    word: Текущее слово
+    theme: Тема слова
+    answer: Загаданное слово
+    status: Статус игры: подготовка, идет игра, ПОБЕДА, проиграли
     """
     def __init__(self, difficulty=1, ):
         self.health = 5
@@ -35,32 +118,66 @@ class HangmanGame:
         self.word = ""
         self.theme = ""
         self.answer = ""
+        self.status = "подготовка"
 
     def start(self):
         """
         Запуск игры
         :return:
         """
+        self.status = "идет игра"
         self.started = True
         self.health = 5
         question_word = choice(WORDS_TUPLE)
-        self.theme = question_word[0]
-        self.answer = question_word[1]
+        self.theme = question_word[0].capitalize()
+        self.answer = question_word[1].lower()
         self.word = "#" * len(self.answer)
 
-    def guess(self, symbol):
+    def guess(self, symbol) -> bool:
+        """
+        Метод для угадывания буквы
+        :param symbol: буква, которую ввел игрок
+        :return: Возвращает истину, если угадал букву, возвразает ложь, если не угадал.
+        """
         if symbol in self.answer:
             new_word = list(self.word)
-            for index in range(len(self.answer)):
-                if self.answer[index] == symbol:
+            for index, element in enumerate(self.answer):
+                if element == symbol:
                     new_word[index] = symbol
             self.word = "".join(new_word)
+            if "#" not in self.word:
+                self.game_over("ПОБЕДА")
+            return True
         else:
             self.health -= 1
             if self.health == 0:
-                self.game_over()
+                self.game_over("проиграли")
+            return False
 
-    def game_over(self):
-        self.started = False
+    def game_over(self, status="подготовка"):
+        """
+        Метод закачивает игру
+        :return:
+        """
         self.health = 0
+        self.status = status
 
+    def show_hangman(self):
+        """
+        Отрисовка висельника из символов
+        :return:
+        """
+
+        match self.health:
+            case 4:
+                return HANGMAN1_STR
+            case 3:
+                return HANGMAN2_STR
+            case 2:
+                return HANGMAN3_STR
+            case 1:
+                return HANGMAN4_STR
+            case 0:
+                return HANGMAN5_STR
+
+        return ""
