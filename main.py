d7081b35dabea4b8a47863435828195ad882a225
Daniil <dan_mishka@mail.ru>
2024-02-05 22:35:20 +1000

    In main add gameplay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from hangman_game import GAME_THEME
+from hangman_game import HangmanGame
 
 
 def main():
@@ -6,16 +6,33 @@ def main():
     Точка входа в приложение
     :return:
     """
-    game_over = False
-    print("Игра начинается \n"
-          f"Тема игры: {GAME_THEME}")
+    while True:
+        game_over = False
+        game = HangmanGame()
+        game.start()
+        print("Игра начинается \n"
+              f"Тема игры: {game.theme}\n"
+              f"Отгадайте слово: {game.word}")
+        while not game_over:
+            symbol = input("Введите букву: \n")
+            if len(symbol) == 1 and symbol.isalpha():
+                symbol = symbol.lower()
+                if game.guess(symbol):
+                    print(game.word)
+                else:
+                    print("Вы не угадали\n"
+                          f"Слово: {game.word}\n")
+                    print(game.show_hangman())
 
-    while not game_over:
-        symbol = input("Введите букву: \n")
-        if len(symbol) == 1 and symbol.isalpha():
-            ...
-        else:
-            print("Введите, пожалуйста, только одну букву!")
+                if game.status == "ПОБЕДА":
+                    print("\nВы выиграли")
+                    game_over = True
+                elif game.status == "проиграли":
+                    print("\nВы проиграли")
+                    game_over = True
+
+            else:
+                print("Введите, пожалуйста, только одну букву!")
 
 
 if __name__ == "__main__":
