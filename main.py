720df7f79c92a4a6b2e627c723d01196b536db62
Daniil <dan_mishka@mail.ru>
2024-02-12 22:13:38 +1000

    Add functional for guess complete word.

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 from hangman_game import HangmanGame
 
+def play_hangman(symbol,game, is_symbol):
+    """
+
+    Шаг игры Hangman
+    :param symbol: Загаданное слово или буква
+    :param game: объект типа HangmanGame
+    :param is_symbol: является ли введёное буквой
+    :return:
+    """
+
+    if is_symbol:
+        guess_function = game.guess
+    else:
+        guess_function = game.guess_word
+
+    if guess_function(symbol):
+        print(game.word)
+    else:
+        print("Вы не угадали\n"
+              f"Слово: {game.word}\n")
+        print(game.show_hangman())
+
+    if game.status == "ПОБЕДА":
+        print("\nВы выиграли")
+        game_over = True
+    elif game.status == "проиграли":
+        print("\nВы проиграли")
+        game_over = True
+
 
 def main():
     """
@@ -14,25 +43,15 @@ def main():
               f"Тема игры: {game.theme}\n"
               f"Отгадайте слово: {game.word}")
         while not game_over:
-            symbol = input("Введите букву: \n")
+            symbol = input("Введите букву или слово целиком: \n")
+            symbol = symbol.lower()
+
             if len(symbol) == 1 and symbol.isalpha():
-                symbol = symbol.lower()
-                if game.guess(symbol):
-                    print(game.word)
-                else:
-                    print("Вы не угадали\n"
-                          f"Слово: {game.word}\n")
-                    print(game.show_hangman())
-
-                if game.status == "ПОБЕДА":
-                    print("\nВы выиграли")
-                    game_over = True
-                elif game.status == "проиграли":
-                    print("\nВы проиграли")
-                    game_over = True
-
+                play_hangman(symbol,game,True)
+            elif len(symbol) > 1 and symbol.isalpha():
+                play_hangman(symbol, game, False)
             else:
-                print("Введите, пожалуйста, только одну букву!")
+                print("Введите, пожалуйста, букву или целое слово!")
 
 
 if __name__ == "__main__":
